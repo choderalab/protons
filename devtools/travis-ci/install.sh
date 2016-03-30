379b019af07a01f70ae17568bb790bc814199c77
Bas Rustenburg <bas.rustenburg@choderalab.org>
2016-03-30 15:40:48 -0400

    Switch to miniconda3.
    To fix md5 mismatch.

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -3,8 +3,8 @@ pushd .
 cd $HOME
 
 # Install Miniconda
-MINICONDA=Miniconda-latest-Linux-x86_64.sh
-MINICONDA_HOME=$HOME/miniconda
+MINICONDA=Miniconda3-latest-Linux-x86_64.sh
+MINICONDA_HOME=$HOME/miniconda3
 MINICONDA_MD5=$(curl -s http://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
 wget -q http://repo.continuum.io/miniconda/$MINICONDA
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
