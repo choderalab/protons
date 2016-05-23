d2f7280c3a0fdcb50fed08198b545e8fcb09e6fa
Bas Rustenburg <bas.rustenburg@choderalab.org>
2016-05-23 14:44:35 -0400

    Minor change to hacky test script. Will need to now implement ffxml reader for the MonteCarloTitration class.

--- constph/examples/ligand.py
+++ constph/examples/ligand.py
@@ -1,25 +1,23 @@
 """Example ligand parametrization script."""
-from constph.ligutils import parametrize_ligand, _TitratableForceFieldCompiler
+from constph.ligutils import parametrize_ligand
 from constph.tests import get_data
 from constph.logger import logger
+from openmmtools.integrators import VelocityVerletIntegrator
 from simtk import openmm, unit
 from simtk.openmm import app
 from sys import stdout
-import logging
+import logging, os
 
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 
-outfile = parametrize_ligand(get_data("dasatinib_allH.mol2", "testsystems"), "ligand-isomers.xml", pH=7.4, max_antechambers=4)
-# _TitratableForceFieldCompiler(get_data("intermediate.xml", "testsystems")).write("ligand-isomers.xml")
-pdb = app.PDBFile(get_data("dasatinib_allH.pdb", "testsystems"))
+outfile = parametrize_ligand(get_data("imidazole.mol2", "testsystems"), "ligand-isomers.xml", pH=7.4, max_antechambers=1)
+pdb = app.PDBFile(get_data("imidazole.pdb", "testsystems"))
 forcefield = app.ForceField("ligand-isomers.xml")
 
 system = forcefield.createSystem(pdb.topology, nonbondedMethod=app.NoCutoff, constraints=app.HBonds)
-#
-integrator = openmm.LangevinIntegrator(300*unit.kelvin, 1.0/unit.picoseconds,
-    1.0*unit.femtoseconds)
-integrator.setConstraintTolerance(0.00001)
+
+integrator = VelocityVerletIntegrator(1.0*unit.femtoseconds)
 
 platform = openmm.Platform.getPlatformByName('CPU')
 
@@ -27,14 +25,12 @@ simulation = app.Simulation(pdb.topology, system, integrator, platform,)
 simulation.context.setPositions(pdb.positions)
 
 print('Minimizing...')
-simulation.minimizeEnergy()
+simulation.minimizeEnergy(tolerance=0.001*unit.kilojoule/unit.mole)
 
-simulation.context.setVelocitiesToTemperature(300*unit.kelvin)
+simulation.context.setVelocitiesToTemperature(300.0*unit.kelvin)
 print('Equilibrating...')
-simulation.step(5000)
-
-simulation.reporters.append(app.DCDReporter('trajectory.dcd', 1000))
-simulation.reporters.append(app.StateDataReporter(stdout, 1000, step=True,
+simulation.reporters.append(app.PDBReporter('trajectory.pdb', 1))
+simulation.reporters.append(app.StateDataReporter(stdout, 1, step=True,
     potentialEnergy=True, temperature=True, progress=True, remainingTime=True,
     speed=True, totalSteps=1000, separator='\t'))
 
