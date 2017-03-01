438a4f458c35ef815a32afc7d0a41f2e0fda6566
Bas Rustenburg <bas.rustenburg@choderalab.org>
2017-03-01 16:36:51 -0500

    Change call to split
    Why:
    
    * Split takes no keyword arguments in python 2.7.
    
    This change addresses the need by:
    
    * Use positional argument instead.

--- protons/forcefields/Amber_input_files/ions/1-compile_ion_ffxml.py
+++ protons/forcefields/Amber_input_files/ions/1-compile_ion_ffxml.py
@@ -6,7 +6,7 @@ lib = "Amber_input_files/ions/atomic_ions.lib"
 
 frcmods = glob("Amber_input_files/ions/frcmod*")
 for frcmod in frcmods:
-    solvent_model = frcmod.split(sep="_")[-1]
+    solvent_model = frcmod.split("_")[-1]
     amberset = AmberParameterSet(lib, frcmod)
     openmmset = OpenMMParameterSet.from_parameterset(amberset)
     openmmset.write("Amber_input_files/ions/raw_ions_{}-tmp.xml".format(solvent_model))
