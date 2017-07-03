727ff988fcf9f644603fef8ada7d3d55d3e68425
Bas Rustenburg <bas.rustenburg@choderalab.org>
2017-07-03 17:48:30 -0400

    Fix reference to Forcefield
    Why:
    
    Custom ForceField class was removed in favor of using data directories.

--- protons/app/__init__.py
+++ protons/app/__init__.py
@@ -5,8 +5,8 @@ from __future__ import print_function
 
 from simtk.openmm.app import *
 from .topology import Topology
+from . import forcefield
 from .calibration import SelfAdjustedMixtureSampling
-from .forcefield import ForceField
 from .simulation import ConstantPHCalibration, ConstantPHSimulation
 from .driver import ForceFieldProtonDrive, AmberProtonDrive
 from .proposals import UniformProposal, DoubleProposal, CategoricalProposal
