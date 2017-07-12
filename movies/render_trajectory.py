6a9a9e5d35700847ffecdd95f6d28beeb7d7ebbe
John Chodera <john.chodera@choderalab.org>
2017-07-11 23:01:38 -0600

    Updates to movie script for imatinib in solvent

--- movies/render_trajectory.py
+++ movies/render_trajectory.py
@@ -27,6 +27,7 @@ import mdtraj
 #=============================================================================================
 
 system = 'imatinib' # 'imatinib'
+#system = 'abl-imatinib-full' # 'imatinib'
 png_dir = 'png' # BE CAREFUL: This directory will be removed every time you run this
 
 if system == 'imatinib':
@@ -34,6 +35,11 @@ if system == 'imatinib':
     netcdf_filename = '%s/Imatinib-solvent.nc' % prefix
     trajectory_filename = '%s/Imatinib-solvent.dcd' % prefix
     reference_pdb_filename = '%s/imatinib-solvated-for-calibration.pdb' % prefix
+elif system == 'abl-imatinib-full':
+    prefix = 'GBAOAB/Abl-Imatinib-full'
+    netcdf_filename = '%s/Abl-Imatinib-full.nc' % prefix
+    trajectory_filename = '%s/Abl-Imatinib-full.dcd' % prefix
+    reference_pdb_filename = '%s/2HYY-H.pdb' % prefix
 else:
     raise Exception('System %s unrecognized' % system)
 
@@ -129,37 +135,80 @@ cmd.mdelete("1")
 
 #cmd.orient('solute')
 util.cbaw('solute')
-cmd.show('spheres', 'solute') # DNA
+cmd.show('spheres', 'solute')
 
 # Render movie
 #cmd.set('ray_trace_frames', 1)
-#nframes = 2
+#nframes = 100
+npause = 30
+output_frame = 0
+cmd.set('suspend_updates', 'off')
+last_active_protons = list()
+last_inactive_protons = list()
+
 for frame in range(nframes):
     print "rendering frame %04d / %04d" % (frame+1, nframes)
-    cmd.set('suspend_updates', 'on')
     cmd.frame(frame+1) # update frames
     # Determine proton indices that should be on and off
     active_protons = list()
     inactive_protons = list()
 
-    cmd.show('spheres', 'solute')
+    #cmd.show('spheres', 'solute')
+    #cmd.set('sphere_transparency', 0.0)
     for residue in range(nresidues):
         for atom in range(natoms):
             if atom_status[frame,residue,atom] == 0:
                 inactive_protons.append(atom_index[residue,atom] + 1)
             elif atom_status[frame,residue,atom] == 1:
                 active_protons.append(atom_index[residue,atom] + 1)
+
+    appear_set = set(active_protons).intersection(last_inactive_protons)
+    disappear_set = set(inactive_protons).intersection(last_active_protons)
+
     active_selection = 'id ' + '+'.join([str(index) for index in active_protons])
     inactive_selection = 'id ' + '+'.join([str(index) for index in inactive_protons])
+
+    appear_selection = 'id ' + '+'.join([str(index) for index in appear_set])
+    disappear_selection = 'id ' + '+'.join([str(index) for index in disappear_set])
+
+    if frame == 0:
+        cmd.set('sphere_transparency', 0, active_selection)
+        cmd.set('sphere_transparency', 1, inactive_selection)
+
+    if len(disappear_set) > 0:
+        for pause_frame in range(npause):
+            cmd.set('sphere_transparency', float(pause_frame+1)/float(npause), disappear_selection)
+            filename = os.path.join(png_dir, 'frame%05d.png' % output_frame)
+            cmd.png(filename)
+            output_frame += 1
+
     #cmd.show('spheres', active_selection)
-    cmd.hide('spheres', inactive_selection)
+    #cmd.hide('spheres', inactive_selection)
+    #cmd.set('sphere_transparency', 0.0)
     #cmd.set('sphere_transparency', 0.8, inactive_selection)
     #cmd.set('sphere_transparency', 0.0, active_selection)
+
+    if len(appear_set) > 0:
+        for pause_frame in range(npause):
+            cmd.set('sphere_transparency', 1.0 - float(pause_frame+1)/float(npause), appear_selection)
+            filename = os.path.join(png_dir, 'frame%05d.png' % output_frame)
+            cmd.png(filename)
+            output_frame += 1
+
+    #if nchange > 0:
+    #    for pause_frame in range(npause):
+    #        filename = os.path.join(png_dir, 'frame%05d.png' % output_frame)
+    #        cmd.png(filename)
+    #        output_frame += 1
+    #        cmd.set('stick_transparency', 1.0 - float(pause_frame+1)/float(nframes), disappear_selection)
+    #        cmd.set('stick_transparency', float(pause_frame+1)/float(nframes), appear_selection)
+
 
     last_inactive_protons = inactive_protons
     last_active_protons = active_protons
 
-    filename = os.path.join(png_dir, 'frame%05d.png' % frame)
-    print(filename)
-    cmd.set('suspend_updates', 'off')
-    cmd.png(filename)
+    if len(appear_set) + len(disappear_set) == 0:
+        filename = os.path.join(png_dir, 'frame%05d.png' % output_frame)
+        cmd.set('suspend_updates', 'off')
+        cmd.png(filename)
+        output_frame += 1
