3dc132356eb4a459fbeb3cdb6871a9313f827dd3
Bas Rustenburg <bas.rustenburg@choderalab.org>
2017-09-19 16:43:14 -0400

    Extend export/import of calibration properties
    Why:
    
    * While previously possible for some properties, it wasn't possible without
    a hack to export a complete state of the Calibration object. I've added a
    method that cleanly exports the state of a calibration object, including
    settings that are necessery to resume a calibration.
    
    This change addresses the need by:
    
    * Extending the variables that are read from samsProperties in the init of
      ConstantPHCalibration
    
    * Adding an export_samsProperties method that returns a dictionary that
    can be used in a resumed simulation.

--- protons/tests/test_simulation.py
+++ protons/tests/test_simulation.py
@@ -135,6 +135,71 @@ class TestConstantPHCalibration:
         # Adapt the weights using binary update.
         simulation.adapt()
 
+    def test_create_constantphcalibration_resume(self):
+        """Test running a calibration using constant-pH."""
+        pdb = app.PDBxFile(get_test_data('glu_ala_his-solvated-minimized-renamed.cif', 'testsystems/tripeptides'))
+        forcefield = app.ForceField('amber10-constph.xml', 'ions_tip3p.xml', 'tip3p.xml')
+
+        system = forcefield.createSystem(pdb.topology, nonbondedMethod=app.PME,
+                                         nonbondedCutoff=1.0 * unit.nanometers, constraints=app.HBonds, rigidWater=True,
+                                         ewaldErrorTolerance=0.0005)
+
+        temperature = 300 * unit.kelvin
+        integrator = GBAOABIntegrator(temperature=temperature, collision_rate=1.0 / unit.picoseconds,
+                                      timestep=2.0 * unit.femtoseconds, constraint_tolerance=1.e-7, external_work=False)
+        ncmcintegrator = GBAOABIntegrator(temperature=temperature, collision_rate=1.0 / unit.picoseconds,
+                                          timestep=2.0 * unit.femtoseconds, constraint_tolerance=1.e-7,
+                                          external_work=True)
+
+        compound_integrator = mm.CompoundIntegrator()
+        compound_integrator.addIntegrator(integrator)
+        compound_integrator.addIntegrator(ncmcintegrator)
+        pressure = 1.0 * unit.atmosphere
+
+        system.addForce(mm.MonteCarloBarostat(pressure, temperature))
+        driver = ForceFieldProtonDrive(temperature, pdb.topology, system, forcefield, ['amber10-constph.xml'],
+                                       pressure=pressure,
+                                       perturbations_per_trial=0)
+        simulation = app.ConstantPHCalibration(pdb.topology, system, compound_integrator, driver, group_index=1,
+                                               platform=self._default_platform)
+        simulation.context.setPositions(pdb.positions)
+        simulation.context.setVelocitiesToTemperature(temperature)
+        simulation.step(1)
+        # Update the titration states using the uniform proposal
+        simulation.update(1)
+        # Adapt the weights using binary update, a few times just to rack up the counters.
+        for x in range(5):
+            simulation.adapt()
+        # retrieve the samsProperties
+        samsProperties = simulation.export_samsProperties()
+
+        integrator2 = GBAOABIntegrator(temperature=temperature, collision_rate=1.0 / unit.picoseconds,
+                                      timestep=2.0 * unit.femtoseconds, constraint_tolerance=1.e-7, external_work=False)
+        ncmcintegrator2 = GBAOABIntegrator(temperature=temperature, collision_rate=1.0 / unit.picoseconds,
+                                          timestep=2.0 * unit.femtoseconds, constraint_tolerance=1.e-7,
+                                          external_work=True)
+        compound_integrator2 = mm.CompoundIntegrator()
+        compound_integrator2.addIntegrator(integrator2)
+        compound_integrator2.addIntegrator(ncmcintegrator2)
+
+        # Make a new calibration and do another step, ignore the state for this example
+        simulation2 = app.ConstantPHCalibration(pdb.topology, system, compound_integrator2, driver, group_index=1,
+                                               platform=self._default_platform, samsProperties=samsProperties)
+
+        assert simulation2.stage == simulation.stage
+        # get going then
+        simulation2.context.setPositions(pdb.positions)
+        simulation2.context.setVelocitiesToTemperature(temperature)
+        simulation2.step(1)
+        # Update the titration states using the uniform proposal
+        simulation2.update(1)
+        # Adapt the weights using binary update.
+        simulation2.adapt()
+
+        assert simulation2.current_adaptation == 6, "The resumed calibration does not have the right adaptation_index"
+
+        import pytest; pytest.set_trace()
+
     def test_create_constantphcalibration_with_reporters(self):
         """Test running a calibration using constant-pH with reporters."""
         pdb = app.PDBxFile(get_test_data('glu_ala_his-solvated-minimized-renamed.cif', 'testsystems/tripeptides'))
