a090f9050d31f7df9d3f6ed462b52e10d643db0b
Bas Rustenburg <bas.rustenburg@choderalab.org>
2018-03-23 18:26:44 -0400

    Fix major bug in ncmc protocol for ions
    Why:
    
    Ions were not being added to the ncmc protocol by mistake.
    
    What:
    
    The call to _perform_ncmc_protocol has been updated to now actually take ions into account.

--- protons/app/driver.py
+++ protons/app/driver.py
@@ -1842,7 +1842,10 @@ class NCMCProtonDrive(_BaseDrive):
                 # Only perform NCMC when the proposed state is different from the current state
                 if initial_titration_states != final_titration_states:
                     # Run NCMC integration.
-                    work = self._perform_ncmc_protocol(titration_group_indices, initial_titration_states, final_titration_states)
+                    if self.swapper:
+                        self._perform_ncmc_protocol(titration_group_indices, initial_titration_states, final_titration_states, saltswap_residue_indices, saltswap_states)
+                    else:
+                        work = 
                 else:
                     work = 0.0
                     for step in range(self.perturbations_per_trial):
